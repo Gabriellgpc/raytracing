fb7c6047b10e76c62bcaf273cb668153e7b81983
heitordnts <heitor.dantas17@gmail.com>
2019-11-17 19:53:03 -0300

    portando pra glm v 0.9.7

--- main.cpp
+++ main.cpp
@@ -116,7 +116,8 @@ void spinLight()
   model = glm::rotate(model, STEP_LIGHT_R, axisY);
 
   posLight = model*posLight;
-  raytracer.world.lights.begin()->pos = posLight;
+  Vec aux(posLight.x,posLight.z,posLight.z);
+  raytracer.world.lights.begin()->pos = aux;
 
   display();
 }
